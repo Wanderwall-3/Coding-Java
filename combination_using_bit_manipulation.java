f453acea2b5c2518bceb992a73f3f3398eb3269a
Vignesh Sathya <118740959+Wanderwall-3@users.noreply.github.com>
2024-09-10 22:07:11 +0530

    Update and rename combination.java to combination_using_bit_manipulation.java

--- combination_using_bit_manipulation.java
+++ combination_using_bit_manipulation.java
@@ -1,15 +1,15 @@
 package day2;   //not completed
 import java.util.*;
-public class combination {
+public class combination_using_bit_manipulation {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s=sc.next();
         int sl=s.length();
         int r=(1<<sl);
-        for(int i=1;i<r;i++){
+        for(int i=1;i<r;i++){                //IT WORKS ONLY Z POWER N -1 FOR PRINT ALL THE COMBINATIONS.
             int w=i;
             for(int j=0;j<sl;j++){
-                if((1&(w>>j))==1){
+                if((1&(w>>j))==1){            //iDENTIFY THE ONE BIT AND PRINT THAT PARTICULAR CHARACTER CORREPONDING TO IT.
                     System.out.print(s.charAt(j));
                 }
             }
